2f46268a5cc16bc514229ad7d2e295b0c1b91558
Mehedi Hasan Majed <mdmajedhasan599@gmail.com>
2022-05-28 20:00:57 +0600

    Delete product route added

--- index.js
+++ index.js
@@ -60,6 +60,12 @@ async function run() {
             const result = await partsCollection.updateOne(filter, updatedDoc)
             res.send(result)
         })
+        app.delete("/parts/:id", async (req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectId(id) }
+            const result = await partsCollection.deleteOne(query)
+            res.send(result)
+        })
 
         // Orders Routes
         app.get("/orders", async (req, res) => {
